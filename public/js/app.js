247f981ebc06e7b889e4fea71d6c56b1c743f190
alaamahasneh <alaa.ibraheem97@yahoo.com>
2020-01-05 16:28:05 +0200

    its working

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 'use strict';
-    // $("#selectBook").click(function() {
-    //   $("<%=book.id%>").show();
-    //   $('.div').hide();
-    // });
+    
     $('#<%= book.etag %>').click(showForm);
     function showForm(){
       $('#<%= book.id %>').show() ;
